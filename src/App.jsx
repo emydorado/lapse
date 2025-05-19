72f27b7e883b43b8d752140cef9f02b5f3f578a4
emydorado <145810874+emydorado@users.noreply.github.com>
2025-05-19 13:03:29 -0500

    rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Welcome from './pages/welcome/Welcome';
+import Register from './pages/register/Register';
+import Login from './pages/login/Login';
+import OnBoarding from './pages/onBoarding/OnBoarding';
+import Intro from './pages/intro/Intro';
 import './App.css';
 
 function App() {
-	return <></>;
+	return (
+		<>
+			<BrowserRouter>
+				<Routes>
+					<Route path='/' element={<Welcome />} />
+					<Route path='/register' element={<Register />} />
+					<Route path='/login' element={<Login />} />
+					<Route path='/onBoarding' element={<OnBoarding />} />
+					<Route path='/intro' element={<Intro />} />
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
 }
 
 export default App;
---
+++ src/pages/onBoarding/OnBoarding.jsx
@@ -0,0 +1,5 @@
+function OnBoarding() {
+	return <></>;
+}
+
+export default OnBoarding;
