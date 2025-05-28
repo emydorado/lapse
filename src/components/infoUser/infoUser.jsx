8763ca3ed65cdfcf8bda05ad0ba3139acedda10b
emydorado <145810874+emydorado@users.noreply.github.com>
2025-05-28 17:29:34 -0500

    detalles profile

--- src/components/infoUser/infoUser.jsx
+++ src/components/infoUser/infoUser.jsx
@@ -1,5 +1,4 @@
 import { useNavigate } from 'react-router-dom';
-
 import './infoUser.css';
 
 function InfoUser({ name, mail, estatura, peso, edad }) {
@@ -9,33 +8,56 @@ function InfoUser({ name, mail, estatura, peso, edad }) {
 	return (
 		<div id='info-user-card'>
 			<div className='user-name'>
-				<h3>Nombre</h3>
-				<p>{name}</p>
+				<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 448 512' width='20' height='20' fill='#272d29'>
+					<path d='M304 128a80 80 0 1 0 -160 0 80 80 0 1 0 160 0zM96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM49.3 464l349.5 0c-8.9-63.3-63.3-112-129-112l-91.4 0c-65.7 0-120.1 48.7-129 112zM0 482.3C0 383.8 79.8 304 178.3 304l91.4 0C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7L29.7 512C13.3 512 0 498.7 0 482.3z' />
+				</svg>
+				<p className='regular-text'>{name}</p>
 			</div>
 
 			<div className='user-mail'>
-				<h3>Correo</h3>
-				<p>{mail}</p>
+				<svg
+					class='w-6 h-6 text-gray-800 dark:text-white'
+					aria-hidden='true'
+					xmlns='http://www.w3.org/2000/svg'
+					width='20'
+					height='20'
+					fill='none'
+					viewBox='0 0 24 24'
+				>
+					<path
+						stroke='#272d29'
+						strokeLinecap='round'
+						strokeWidth='2'
+						d='m3.5 5.5 7.893 6.036a1 1 0 0 0 1.214 0L20.5 5.5M4 19h16a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Z'
+					/>
+				</svg>
+
+				<p className='regular-text'>{mail}</p>
 			</div>
 
-      <div className='aditional-info'>
+			<div className='aditional-info'>
+				<div className='user-edad'>
+					<p className='regular-text'>
+						<b>Edad:</b>
+					</p>
+					<p className='regular-text'>{edad}</p>
+				</div>
 
-			<div className='user-edad'>
-				<h3>Edad</h3>
-				<p>{edad}</p>
-			</div>
+				<div className='user-estatura'>
+					<p className='regular-text'>
+						<b>Estatura:</b>
+					</p>
+					<p className='regular-text'>{estatura}cm</p>
+				</div>
 
-			<div className='user-estatura'>
-				<h3>Estatura</h3>
-				<p>{estatura}</p>
-			</div>
-
-			<div className='user-peso'>
-				<h3>Peso</h3>
-				<p>{peso}</p>
+				<div className='user-peso'>
+					<p className='regular-text'>
+						<b>Peso:</b>
+					</p>
+					<p className='regular-text'>{peso}kg</p>
+				</div>
 			</div>
 		</div>
-    </div>
 	);
 }
 
--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -11,16 +11,19 @@ function Profile() {
 
 	return (
 		<>
+			<div className='home-header'>
+				<img src='../../src/assets/logo.png' alt='logo lapse' />
+			</div>
 			<div id='profile-container'>
 				<div id='info-user-container'>
 					<div className='tittle-container1'>
 						<h3>Tus datos</h3>
 						<svg
-							className='w-6 h-6 text-gray-800 dark:text-white'
+							class='w-6 h-6 text-gray-800 dark:text-white'
 							aria-hidden='true'
 							xmlns='http://www.w3.org/2000/svg'
-							width='24'
-							height='24'
+							width='30'
+							height='30'
 							fill='none'
 							viewBox='0 0 24 24'
 						>
@@ -28,8 +31,8 @@ function Profile() {
 								stroke='currentColor'
 								strokeLinecap='round'
 								strokeLinejoin='round'
-								strokeWidth='2'
-								d='M5 12h14m-7 7V5'
+								strokeWidth='1.5'
+								d='m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z'
 							/>
 						</svg>
 					</div>
@@ -38,7 +41,6 @@ function Profile() {
 
 				<div className='sugestions-container'>
 					<div className='tittle-container2'>
-						{' '}
 						<h3>tus rutinas</h3>
 						<svg
 							className='w-6 h-6 text-gray-800 dark:text-white'
