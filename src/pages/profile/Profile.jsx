f83510f46753ccdc4aca931a0ff443b17ad9c07b
SofiRn <sofiaromeronar@gmail.com>
2025-05-27 22:09:02 -0500

    profile css

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,6 +1,9 @@
 import { useNavigate } from 'react-router-dom';
-import NavMenu from '../../components/navMenu/NavMenu';
+import NavMenu from '../../components/navMenu/NavMenu.jsx';
 import './profile.css';
+import { routines } from '../../data/routines.js';
+import RoutineCard from '../../components/routineCard/RoutineCard';
+import InfoUser from '../../components/infoUser/infoUser.jsx';
 
 function Profile() {
 	const navigate = useNavigate();
@@ -9,7 +12,69 @@ function Profile() {
 	return (
 		<>
 			<div id='profile-container'>
-				<h1>profile</h1>
+				<div id='info-user-container'>
+					<div className='tittle-container1'>
+						<h3>Tus datos</h3>
+						<svg
+							className='w-6 h-6 text-gray-800 dark:text-white'
+							aria-hidden='true'
+							xmlns='http://www.w3.org/2000/svg'
+							width='24'
+							height='24'
+							fill='none'
+							viewBox='0 0 24 24'
+						>
+							<path
+								stroke='currentColor'
+								strokeLinecap='round'
+								strokeLinejoin='round'
+								strokeWidth='2'
+								d='M5 12h14m-7 7V5'
+							/>
+						</svg>
+					</div>
+					<InfoUser name='Sofía Romero' mail='sofiaromeronar@gmail.com' edad='23' estatura='165' peso='50' />
+				</div>
+
+				<div className='sugestions-container'>
+					<div className='tittle-container2'>
+						{' '}
+						<h3>tus rutinas</h3>
+						<svg
+							className='w-6 h-6 text-gray-800 dark:text-white'
+							aria-hidden='true'
+							xmlns='http://www.w3.org/2000/svg'
+							width='24'
+							height='24'
+							fill='none'
+							viewBox='0 0 24 24'
+						>
+							<path
+								stroke='currentColor'
+								strokeLinecap='round'
+								strokeLinejoin='round'
+								strokeWidth='2'
+								d='M5 12h14m-7 7V5'
+							/>
+						</svg>
+					</div>
+					<div className='routine-card'>
+						{routines.map((routine) => (
+							<RoutineCard
+								key={routine.id}
+								name={routine.name}
+								img={routine.img}
+								number_excercises={routine.number_excercises}
+								id={routine.id}
+							/>
+						))}
+					</div>
+					<p className='regular-text'>
+						<b>
+							<u>Ver más</u>
+						</b>
+					</p>
+				</div>
 			</div>
 			<NavMenu />
 		</>
